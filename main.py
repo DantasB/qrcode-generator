ef44090f8d371d5e1ce906ed2e9ea7c15925a6be
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-27 14:23:39 -0300

    Creating main function on main.py

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from SharedLibrary.csv_reader_utils import csv_to_dataframe
 from SharedLibrary.qrcode_utils import get_qrcode
 
 
-if __name__ == '__main__':
+def main() -> None:
     qrcode_folder, csv_path = get_env_parameters()
     df = csv_to_dataframe(csv_path)
     for index, row in df.iterrows():
         get_qrcode(qrcode_folder, row['URL'], row['Category'])
+
+
+if __name__ == '__main__':
+    main()
